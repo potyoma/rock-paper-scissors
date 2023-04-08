91f1ec687f9f49e7d572da379ec9997dd7a2fa96
potyoma <artyomnovosyolov254@gmail.com>
2023-04-09 01:29:36 +0400

    feat: meta tags and favicon for seo

--- src/storage/index.ts
+++ src/storage/index.ts
@@ -1,20 +1,28 @@
+const tryAccess = <T>(fn: (storage: Storage) => T | undefined) => {
+  try {
+    const { sessionStorage: storage } = window
+    return fn?.(storage)
+  } catch {
+    return undefined
+  }
+}
+
 const setStoreValue = <T>(key: string, val: T) => {
-  const { localStorage: storage } = window
   const stringified = JSON.stringify(val)
-  storage.setItem(key, stringified)
+  tryAccess(storage => storage.setItem(key, stringified))
 }
 
 const readStoreValue = <T>(key: string): T | undefined => {
-  const { localStorage: storage } = window
-  const value = storage.getItem(key)
+  const value = tryAccess(storage => storage.getItem(key))
 
   if (!value) return undefined
 
   return JSON.parse(value) as T
 }
 
-const clear = () => window.localStorage.clear()
+const clear = () => tryAccess(storage => storage.clear())
 
-const removeItem = (key: string) => window.localStorage.removeItem(key)
+const removeItem = (key: string) =>
+  tryAccess(storage => storage.removeItem(key))
 
 export { setStoreValue, readStoreValue, clear, removeItem }
