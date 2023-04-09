ca903da9a3d47b652138db9c5fee79c345774c82
potyoma <artyomnovosyolov254@gmail.com>
2023-04-09 17:03:40 +0400

    feat: play again mobile

--- src/components/playAgain.tsx
+++ src/components/playAgain.tsx
@@ -1,3 +1,53 @@
-const PlayAgain = () => <></>
+import styled from "styled-components"
+import withFadeShow from "../hoks/withFadeShow"
+import { GameContext, GameStage } from "../contexts/gameContext"
+import Button from "./button"
+import { useContext, useEffect, useState } from "react"
+import { GameResult } from "../core"
 
-export default PlayAgain
+const PlayAgainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const Description = styled.h2`
+  color: var(--white);
+  font-size: 2rem;
+`
+
+const PlayAgain = () => {
+  const [description, setDescription] = useState("")
+
+  const { restore, gameResult } = useContext(GameContext) ?? {}
+
+  const handleClick = () => restore?.()
+
+  useEffect(() => {
+    const getText = (gameResult?: GameResult) => {
+      switch (gameResult) {
+        case GameResult.Draw:
+          return "draw"
+        case GameResult.Win:
+          return "you win"
+        case GameResult.Loose:
+          return "you loose"
+        default:
+          return ""
+      }
+    }
+
+    setDescription(getText(gameResult))
+  }, [gameResult])
+
+  return (
+    <>
+      <Description>{description}</Description>
+      <Button onClick={handleClick} size="lg" filled>
+        Play again
+      </Button>
+    </>
+  )
+}
+
+export default withFadeShow(PlayAgain, GameStage.Finished, PlayAgainContainer)
